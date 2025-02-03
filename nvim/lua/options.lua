fb6207486b06843bab5ef5fa46445c395894e05e
Ryan <ryan.ward17@outlook.com>
2025-02-03 22:25:52 +1000

    backing up dotfiles

---
+++ nvim/lua/options.lua
@@ -0,0 +1,11 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+o.cursorlineopt ='both' -- to enable cursorline!
+o.tabstop = 4
+o.shiftwidth = 4
+o.expandtab = false
+o.smartindent = true
+o.smarttab = true
